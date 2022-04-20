4bcd8bc9e82672eca7409d7f8be4f2f043783f48
RafaelContact <contactrodrigues21@gmail.com>
2022-04-20 01:32:08 -0300

    data response não funciona

---
+++ main.js
@@ -0,0 +1,28 @@
+const linksSocialMedia = {
+    github: 'jakeliny',
+    youtube: 'jakelinygracielly',
+    instagram:  'jakeliny.gracielly',
+    facebook: 'maykbrito',
+    twitter: 'jakelinytec'
+}
+function changeLinksSocialMedia(){
+    for(let li of socialLinks.children){
+       const social = li.getAtribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+      
+    // alert(li.childrem[0].href)
+    }
+}
+// changeLinksSocialMedia()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/RafaelContact${linksSocialMedia.github}`
+// fetch bate na url e pega o o que a api responde e traz
+    
+fetch(url)
+    .then(response => response.json())
+    .them(data => alert(data.bio))
+}
+
+getGitHubProfileInfos()
