bfebecd1acbbd17c7f718691d9a189bb3aa92566
RafaelContact <contactrodrigues21@gmail.com>
2022-04-20 16:42:09 -0300

    finalizando mudanças

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const linksSocialMedia = {
 }
 function changeLinksSocialMedia() {
    for (let li of socialLinks.children){
-//    const social = li.getAttribute('class')
-    li.children[0].href = "https://www.youtube.com/jakelinygracielly"
+   const social = li.getAttribute('class')
+   
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
 
 //    alert(li.children[0].href)
     }
